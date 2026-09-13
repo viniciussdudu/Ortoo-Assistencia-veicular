f4a2dcc023b242919713e4fc6402bb87a3ed5528
Samara Coelho <alien2000futurista@gmail.com>
2026-09-13 15:23:24 -0300

    feat: implementa recuperacao de senha no frontend e backend

--- Server/src/routes/api.js
+++ Server/src/routes/api.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authController = require("../controllers/authController");
-const userController = require("../controllers/userController"); // Supondo que você tenha ou vá criar esse controller
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/auth/cadastro", authController.register);
 router.post("/auth/logout", authController.logout);
+router.post("/auth/recuperar-senha", authController.recuperarSenha);
 
 router.get("/usuario/perfil", userController.getProfile);
 
